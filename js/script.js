0742a1f1e2e6a389dcdccdbd1804ec0d028122f4
Paola Arrieta <vargasp437@gmail.com>
2024-02-16 18:21:55 -0600

    cambios

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 var scroll = window.scrollY;
 
-
 document.addEventListener("DOMContentLoaded", function () {
     // Elementos a animar:
     var corazon = document.querySelector('.corazon');
@@ -34,3 +33,48 @@ document.addEventListener("DOMContentLoaded", function () {
         icon4.style.transform = 'translate3d(0,' + window.scrollY * -0.15 + 'px, 0)';
     });
 });
+
+(function(){
+    init();
+
+    var g_containerInViewport;
+    function init(){
+        setStickyContainersSize();
+        bindEvents();
+    }
+
+    function bindEvents(){
+        window.addEventListener("wheel", wheelHandler);        
+    }
+
+    function setStickyContainersSize(){
+        document.querySelectorAll('.sticky-container').forEach(function(container){
+            const stikyContainerHeight = container.querySelector('main').scrollWidth;
+            container.setAttribute('style', 'height: ' + stikyContainerHeight + 'px');
+        });
+    }
+
+    function isElementInViewport (el) {
+        const rect = el.getBoundingClientRect();
+        return rect.top <= 0 && rect.bottom > document.documentElement.clientHeight;
+    }
+
+    function wheelHandler(evt){
+        
+        const containerInViewPort = Array.from(document.querySelectorAll('.sticky-container')).filter(function(container){
+            return isElementInViewport(container);
+        })[0];
+
+        if(!containerInViewPort){
+            return;
+        }
+
+        var isPlaceHolderBelowTop = containerInViewPort.offsetTop < document.documentElement.scrollTop;
+        var isPlaceHolderBelowBottom = containerInViewPort.offsetTop + containerInViewPort.offsetHeight > document.documentElement.scrollTop;
+        let g_canScrollHorizontally = isPlaceHolderBelowTop && isPlaceHolderBelowBottom;
+
+        if(g_canScrollHorizontally){
+            containerInViewPort.querySelector('main').scrollLeft += evt.deltaY;
+        }
+    }
+})();
